664aa0061156d7b86c0c8b260644e6afd3701f97
Gabriel <gabrielmaranhao10@gmail.com>
2024-04-05 17:17:13 -0300

    update

--- Tools/fix_lvs_klayout.sh
+++ Tools/fix_lvs_klayout.sh
@@ -21,8 +21,17 @@ sed -i '/C.*cap_mim_2f0_m4m5_noshield/s/c_length/L/' "$filename"
 # Modify line containing 'C.*cap_mim_2f0_m4m5_noshield' to replace 'c_width' with 'W'
 sed -i '/C.*cap_mim_2f0_m4m5_noshield/s/c_width/W/' "$filename"
 
+# Modify line containing 'R.*ppoly' to replace 'r_width' with 'W'
+sed -i '/R.*ppoly/s/r_width/W/' "$filename"
+
+# Modify line containing 'R.*ppoly' to replace 'r_length' with 'L'
+sed -i '/R.*ppoly/s/r_length/L/' "$filename"
+
 # Modify line containing 'XC' to replace it with 'C'
 sed -i 's/XC/C/g' "$filename"
+
+# Modify line containing 'XR' to replace it with 'R'
+sed -i 's/XR/R/g' "$filename"
 
 echo "Modifications complete."
 
